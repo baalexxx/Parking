3341d7d3dd333ed1c0d81251733943946da29a9f
baal <baal@belightsoft.com>
2015-12-24 19:44:43 +0100

    Project remade

---
+++ Parking/Parking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parking
+//
+//  Created by Alex Baev on 24/12/15.
+//  Copyright © 2015 baevsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
